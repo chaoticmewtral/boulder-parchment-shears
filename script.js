be3441c0417bdf129c8903c3b02f69bcb0241d91
Remy <remy@chaoticmewtral.com>
2022-09-17 14:44:08 -0400

    Add to script.js

--- script.js
+++ script.js
@@ -1,33 +1,34 @@
 function getComputerChoice() {
     let rps = ['rock', 'paper', 'scissors'];
+
+    return rps[(Math.floor(Math.random() * 2))];
 }
 function playRound(computerSelection, playerSelection)
 
-/*
-computer randomly chooses from ['rock', 'paper', 'scissors']
-user selects from ['rock', 'paper', 'scissors']
-if computer selects rock:
-    if user selects rock
-        tie
-        play again
-    if user selects paper
-        user wins
-    if user selects scissors
-        user loses
-if computer selects paper:
-    if user selects rock
-        user loses
-    if user selects paper
-        tie
-        play again
-    if user selects scissors
-        user wins
-if computer selects scissors:
-    if user selects rock
-        user wins
-    if user selects paper
-        user loses
-    if user selects scissors
-        tie
-        play again
-*/
+// /*
+// computer randomly chooses from ['rock', 'paper', 'scissors']
+// user selects from ['rock', 'paper', 'scissors']
+// if computer selects rock:
+//     if user selects rock
+//         tie
+//         play again
+//     if user selects paper
+//         user wins
+//     if user selects scissors
+//         user loses
+// if computer selects paper:
+//     if user selects rock
+//         user loses
+//     if user selects paper
+//         tie
+//         play again
+//     if user selects scissors
+//         user wins
+// if computer selects scissors:
+//     if user selects rock
+//         user wins
+//     if user selects paper
+//         user loses
+//     if user selects scissors
+//         tie
+//         play again
