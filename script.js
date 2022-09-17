641fdaedb259869386c9031aef601b1b984b0c69
Remy <remy@chaoticmewtral.com>
2022-09-17 14:22:36 -0400

    Add algorith to script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+/*
+computer randomly chooses from ['rock', 'paper', 'scissors']
+user selects from ['rock', 'paper', 'scissors']
+if computer selects rock:
+    if user selects rock
+        tie
+        play again
+    if user selects paper
+        user wins
+    if user selects scissors
+        user loses
+if computer selects paper:
+    if user selects rock
+        user loses
+    if user selects paper
+        tie
+        play again
+    if user selects scissors
+        user wins
+if computer selects scissors:
+    if user selects rock
+        user wins
+    if user selects paper
+        user loses
+    if user selects scissors
+        tie
+        play again
+*/
