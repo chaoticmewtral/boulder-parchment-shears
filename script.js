75653329e7b424f74a4b0917e5f4fa11bb7f55a7
Remy <remy@chaoticmewtral.com>
2022-09-29 16:23:10 -0400

    Remove playRound() from script.js

--- script.js
+++ script.js
@@ -11,78 +11,8 @@ function getComputerChoice() {
 }
 
 function playRound() {
-    computerSelection = getComputerChoice();
-
-    switch(playerSelection) {
-        case 'Boulder':
-            switch(computerSelection) {
-                case 'Boulder':
-                    result[0] = "draw";
-                    return result;
-                    break;
-                    
-                case 'Parchment':
-                    result[0] = "lose";
-                    result[1] = playerSelection;
-                    result[2] = computerSelection;
-                    return result;
-                    break;
-
-                case 'Shears':
-                    result[0] = "win";
-                    result[1] = playerSelection;
-                    result[2] = computerSelection;
-                    return result;
-            }
-            break;
-        case 'Parchment':
-            switch(computerSelection) {
-                case 'Boulder':
-                    result[0] = "win";
-                    result[1] = playerSelection;
-                    result[2] = computerSelection;
-                    return result;
-                    break;
-                    
-                case 'Parchment':
-                    result[0] = "draw";
-                    return result;
-                    break;
-
-                case 'Shears':
-                    result[0] = "lose";
-                    result[1] = playerSelection;
-                    result[2] = computerSelection;
-                    return result;
-            }
-            break;
-            
-        case 'Shears':
-            switch(computerSelection) {
-                case 'Boulder':
-                    result[0] = "lose";
-                    result[1] = playerSelection;
-                    result[2] = computerSelection;
-                    return result;
-                    break;
-                    
-                case 'Parchment':
-                    result[0] = "win";
-                    result[1] = playerSelection;
-                    result[2] = computerSelection;
-                    return result;
-                    break;
-
-                case 'Shears':
-                    result[0] = "draw";
-                    return result;
-            }
-            
-    }
 }
 
 buttons.forEach((button) => {
-    button.addEventListener('click', playRound(button.id) {
-        playerSelection = button.id;
-    });
+    button.addEventListener('click', playRound());
 });
