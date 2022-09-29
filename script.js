09f2f5d4afa140caa7c564a6b235dd8275783047
Remy <remy@chaoticmewtral.com>
2022-09-29 12:45:31 -0400

    Add to script.js

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
+// variables
 const choice = ['Boulder', 'Parchment', 'Shears'];
 const buttons = document.querySelectorAll('button');
+let result = [];
+let playerSelection;
+let computerSelection;
 
+// functions
 function getComputerChoice() {
     return choice[Math.floor(Math.random() * 3)];
 }
 
-const playerSelection = buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-        return button.id;
+function getPlayerChoice() { 
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            return button.id;
+        });
     });
-});
+}
+
+function playRound() {
+    playerSelection = getPlayerChoice();
+    computerSelection = getComputerChoice();
+
+
+}
