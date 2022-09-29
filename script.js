17bac26c531673cf17732b762c308a13e9297d2c
Remy <remy@chaoticmewtral.com>
2022-09-29 12:48:26 -0400

    Capitalize choices in script.js

--- script.js
+++ script.js
@@ -22,5 +22,70 @@ function playRound() {
     playerSelection = getPlayerChoice();
     computerSelection = getComputerChoice();
 
+    switch(playerSelection) {
+        case 'Boulder':
+            switch(computerSelection) {
+                case 'Boulder':
+                    result[0] = "draw";
+                    return result;
+                    break;
+                    
+                case 'Parchment':
+                    result[0] = "lose";
+                    result[1] = playerSelection;
+                    result[2] = computerSelection;
+                    return result;
+                    break;
 
+                case 'Shears':
+                    result[0] = "win";
+                    result[1] = playerSelection;
+                    result[2] = computerSelection;
+                    return result;
+            }
+            break;
+        case 'Parchment':
+            switch(computerSelection) {
+                case 'Boulder':
+                    result[0] = "win";
+                    result[1] = playerSelection;
+                    result[2] = computerSelection;
+                    return result;
+                    break;
+                    
+                case 'Parchment':
+                    result[0] = "draw";
+                    return result;
+                    break;
+
+                case 'Shears':
+                    result[0] = "lose";
+                    result[1] = playerSelection;
+                    result[2] = computerSelection;
+                    return result;
+            }
+            break;
+            
+        case 'Shears':
+            switch(computerSelection) {
+                case 'Boulder':
+                    result[0] = "lose";
+                    result[1] = playerSelection;
+                    result[2] = computerSelection;
+                    return result;
+                    break;
+                    
+                case 'Parchment':
+                    result[0] = "win";
+                    result[1] = playerSelection;
+                    result[2] = computerSelection;
+                    return result;
+                    break;
+
+                case 'Shears':
+                    result[0] = "draw";
+                    return result;
+            }
+            
+    }
 }
