3546e59e336340f44324bceab04a3e7895e2dd8b
Remy <remy@chaoticmewtral.com>
2022-10-04 13:43:40 -0400

    Add to script.js

--- script.js
+++ script.js
@@ -7,81 +7,87 @@ let playerSelection;
 let computerSelection;
 
 // functions
-
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        playerSelection = button.id;
+        computerSelection = getComputerChoice();
+        console.log(playerSelection, computerSelection);
+    });
+});
 
 function getComputerChoice() {
     return choice[Math.floor(Math.random() * 3)];
 }
 
-// function playRound() {
+function playRound() {
 
-//     switch(playerSelection) {
-//         case 'Boulder':
-//             switch(computerSelection) {
-//                 case 'Boulder':
-//                     result[0] = "draw";
-//                     return result;
-//                     break;
+    switch(playerSelection) {
+        case 'Boulder':
+            switch(computerSelection) {
+                case 'Boulder':
+                    result[0] = "draw";
+                    return result;
+                    break;
                 
-//                 case 'Parchment':
-//                     result[0] = "lose";
-//                     result[1] = playerSelection;
-//                     result[2] = computerSelection;
-//                     return result;
-//                     break;
+                case 'Parchment':
+                    result[0] = "lose";
+                    result[1] = playerSelection;
+                    result[2] = computerSelection;
+                    return result;
+                    break;
 
-//                 case 'Shears':
-//                     result[0] = "win";
-//                     result[1] = playerSelection;
-//                     result[2] = computerSelection;
-//                     return result;
-//             }
-//             break;
-//         case 'Parchment':
-//             switch(computerSelection) {
-//                 case 'Boulder':
-//                     result[0] = "win";
-//                     result[1] = playerSelection;
-//                     result[2] = computerSelection;
-//                     return result;
-//                     break;
+                case 'Shears':
+                    result[0] = "win";
+                    result[1] = playerSelection;
+                    result[2] = computerSelection;
+                    return result;
+            }
+            break;
+        case 'Parchment':
+            switch(computerSelection) {
+                case 'Boulder':
+                    result[0] = "win";
+                    result[1] = playerSelection;
+                    result[2] = computerSelection;
+                    return result;
+                    break;
                 
-//                 case 'Parchment':
-//                     result[0] = "draw";
-//                     return result;
-//                     break;
+                case 'Parchment':
+                    result[0] = "draw";
+                    return result;
+                    break;
 
-//                 case 'Shears':
-//                     result[0] = "lose";
-//                     result[1] = playerSelection;
-//                     result[2] = computerSelection;
-//                     return result;
-//             }
-//             break;
+                case 'Shears':
+                    result[0] = "lose";
+                    result[1] = playerSelection;
+                    result[2] = computerSelection;
+                    return result;
+            }
+            break;
         
-//         case 'Shears':
-//             switch(computerSelection) {
-//                 case 'Boulder':
-//                     result[0] = "lose";
-//                     result[1] = playerSelection;
-//                     result[2] = computerSelection;
-//                     return result;
-//                     break;
+        case 'Shears':
+            switch(computerSelection) {
+                case 'Boulder':
+                    result[0] = "lose";
+                    result[1] = playerSelection;
+                    result[2] = computerSelection;
+                    return result;
+                    break;
                 
-//                 case 'Parchment':
-//                     result[0] = "win";
-//                     result[1] = playerSelection;
-//                     result[2] = computerSelection;
-//                     return result;
-//                     break;
+                case 'Parchment':
+                    result[0] = "win";
+                    result[1] = playerSelection;
+                    result[2] = computerSelection;
+                    return result;
+                    break;
 
-//                 case 'Shears':
-//                     result[0] = "draw";
-//                     return result;
-//             }
-//     }
-//     console.log(result);
-// }
+                case 'Shears':
+                    result[0] = "draw";
+                    return result;
+            }
+    }
+    console.log(result);
+}
 
 
 
